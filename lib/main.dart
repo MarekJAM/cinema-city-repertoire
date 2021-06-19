14404207419d12d2650c6b226be7342833dc5ec6
MarekJAM <marekash@gmail.com>
2021-06-19 22:01:34 +0200

    Got rid of english locale

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,6 @@ class App extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: [
-        const Locale('en'),
         const Locale('pl'),
       ],
       title: 'Cinema City Repertuar',
