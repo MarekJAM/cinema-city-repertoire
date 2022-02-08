691dfadbfb8810992714fa0e920919c5c272223a
MarekJAM <marekash@gmail.com>
2022-02-08 23:03:36 +0100

    Progress on filters

--- lib/UI/screens/filters_screen.dart
+++ lib/UI/screens/filters_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 import '../../data/models/film.dart';
+import '../../data/models/event.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen();
@@ -13,32 +14,51 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _genres = genreMap.values.map((val) => MultiSelectItem(val, val)).toList();
+  var _eventTypes = eventTypes.map((val) => MultiSelectItem(val, val)).toList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Container(
-            decoration: BoxDecoration(),
-            child: MultiSelectDialogField(
-              items: _genres,
-              listType: MultiSelectListType.CHIP,
-              initialValue: _genres.map((e) => e.value).toList(),
-              onConfirm: (list) {
-
-              },
-              chipDisplay: MultiSelectChipDisplay(
-                onTap: (value) {
-                  setState(() {
-                    _genres.remove(value);
-                  });
-                },
-                
-              ),
-            ),
+        child: Padding(
+          padding: EdgeInsets.all(5),
+          child: ListView(
+            children: [
+              FilterMultiSelectDialog(title: "Gatunek", values: _genres),
+              FilterMultiSelectDialog(title: "Rodzaj seansu", values: _eventTypes),
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FilterMultiSelectDialog extends StatelessWidget {
+  const FilterMultiSelectDialog({
+    @required this.title,
+    @required this.values
+  });
+
+  final String title;
+  final List<MultiSelectItem<String>> values;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: MultiSelectDialogField(
+        title: Text(title),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.orange[700], width: 1.8),
+        ),
+        buttonText: Text(title),
+        items: values,
+        listType: MultiSelectListType.CHIP,
+        initialValue: values.map((e) => e.value).toList(),
+        autovalidateMode: AutovalidateMode.always,
+        onConfirm: (items) {},
+        chipDisplay: MultiSelectChipDisplay.none(),
       ),
     );
   }
