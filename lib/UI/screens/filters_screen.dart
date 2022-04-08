6057648744b171d1d7780a8da9a8bcf3e871c180
MarekJAM <marekash@gmail.com>
2022-04-08 22:54:58 +0200

    Got rid of multi select package

--- lib/UI/screens/filters_screen.dart
+++ lib/UI/screens/filters_screen.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:multi_select_flutter/multi_select_flutter.dart';
-
 import '../../data/models/models.dart';
 import '../../bloc/blocs.dart';
 
-var _genres = genreMap.values.toList();
+var _genres = genreMap.values.toList()..sort();
 var _eventTypes = eventTypes;
-
-var _pickedGenres = genreMap.values.toList();
-var _pickedEventTypes = eventTypes.toList();
+final List<String> _pickedGenres = [];
+final List<String> _pickedEventTypes = [];
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen();
@@ -20,6 +17,13 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  @override
+  void initState() {
+    _pickedGenres.addAll(_genres);
+    _pickedEventTypes.addAll(eventTypes);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,9 +36,11 @@ class _FiltersScreenState extends State<FiltersScreen> {
                 state.filters.forEach(
                   (filter) {
                     if (filter is GenreFilter) {
-                      _pickedGenres = filter.genres;
+                      _pickedGenres.clear();
+                      _pickedGenres.addAll(filter.genres);
                     } else if (filter is EventTypeFilter) {
-                      _pickedEventTypes = filter.eventTypes;
+                      _pickedEventTypes.clear();
+                      _pickedEventTypes.addAll(filter.eventTypes);
                     }
                   },
                 );
@@ -73,7 +79,7 @@ class _FiltersScreenState extends State<FiltersScreen> {
                           );
                           Navigator.of(context).pop();
                         },
-                        child: Text('Zatwierdź'),
+                        child: Text('Zastosuj'),
                       ),
                     ],
                   )
@@ -101,31 +107,181 @@ class FilterMultiSelectDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 20),
-      child: MultiSelectDialogField(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
         title: Text(title),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: Colors.orange[700], width: 1.8),
-          ),
-        ),
-        buttonText: Text(title),
-        cancelText: Text('Anuluj'),
-        buttonIcon: Icon(Icons.arrow_downward_rounded),
-        selectedColor: Colors.orange,
-        selectedItemsTextStyle: TextStyle(color: Colors.black),
-        items: values.map((e) => MultiSelectItem(e, e)).toList(),
-        listType: MultiSelectListType.CHIP,
-        initialValue: pickedValues,
-        autovalidateMode: AutovalidateMode.always,
-        onConfirm: (items) {
-          pickedValues.clear();
-          items.forEach((el) {
-            pickedValues.add(el);
-          });
+        trailing: Icon(Icons.arrow_drop_down),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => Dialog(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: DialogColumn(title: title, values: values, pickedValues: pickedValues),
+              ),
+            ),
+          );
         },
-        chipDisplay: MultiSelectChipDisplay.none(),
       ),
     );
+  }
+}
+
+class DialogColumn extends StatefulWidget {
+  const DialogColumn({
+    Key key,
+    @required this.title,
+    @required this.values,
+    @required this.pickedValues,
+  }) : super(key: key);
+
+  final String title;
+  final List<String> values;
+  final List<String> pickedValues;
+
+  @override
+  State<DialogColumn> createState() => _DialogColumnState();
+}
+
+class _DialogColumnState extends State<DialogColumn> {
+  List<String> tempPickedValues = [];
+
+  @override
+  void initState() {
+    tempPickedValues.addAll(widget.pickedValues);
+    super.initState();
+  }
+
+  bool _isInitiallySelected(String val) => tempPickedValues.contains(val);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.title,
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.cancel,
+              ),
+            )
+          ],
+        ),
+        Divider(),
+        Wrap(
+          children: [
+            for (int i = 0; i < widget.values.length; i++)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2),
+                child: FilterValueButton(
+                  value: widget.values[i],
+                  pickedValues: tempPickedValues,
+                  tween: ColorTween(
+                    begin: _isInitiallySelected(widget.values[i]) ? Colors.orange : Colors.grey,
+                    end: _isInitiallySelected(widget.values[i]) ? Colors.grey : Colors.orange,
+                  ),
+                ),
+              )
+          ],
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  tempPickedValues.clear();
+                  tempPickedValues.addAll(widget.values);
+                });
+              },
+              child: Text('Reset'),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Zatwierdź'),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class FilterValueButton extends StatefulWidget {
+  const FilterValueButton({
+    Key key,
+    @required this.value,
+    @required this.pickedValues,
+    @required this.tween,
+  }) : super(key: key);
+
+  final String value;
+  final List<String> pickedValues;
+  final ColorTween tween;
+
+  @override
+  State<FilterValueButton> createState() => _FilterValueButtonState();
+}
+
+class _FilterValueButtonState extends State<FilterValueButton> with TickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _colorTween;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _colorTween = widget.tween.animate(_animationController);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) => ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            _colorTween.value,
+          ),
+        ),
+        onPressed: () {
+          if (_animationController.status == AnimationStatus.completed) {
+            _animationController.reverse();
+            toggleValue();
+          } else {
+            _animationController.forward();
+            toggleValue();
+          }
+        },
+        child: Text(
+          widget.value,
+        ),
+      ),
+    );
+  }
+
+  void toggleValue() {
+    if (widget.pickedValues.contains(widget.value)) {
+      widget.pickedValues.remove(widget.value);
+    } else {
+      widget.pickedValues.add(widget.value);
+    }
   }
 }
