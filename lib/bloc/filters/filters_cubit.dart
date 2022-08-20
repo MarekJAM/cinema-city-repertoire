7e9a5e28f99158f13523d77e02a124dd5af38ff3
MarekJAM <marekash@gmail.com>
2022-08-20 22:21:53 +0200

    Added equatable to filters state

--- lib/bloc/filters/filters_cubit.dart
+++ lib/bloc/filters/filters_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/repositories/repositories.dart';
--- lib/bloc/filters/filters_state.dart
+++ lib/bloc/filters/filters_state.dart
@@ -1,7 +1,10 @@
 part of 'filters_cubit.dart';
 
-abstract class FiltersState {
+abstract class FiltersState extends Equatable {
   const FiltersState();
+
+  @override
+  List<Object?> get props => [];
 }
 
 class FiltersInitial extends FiltersState {}
@@ -10,4 +13,7 @@ class FiltersLoaded extends FiltersState {
   final List<RepertoireFilter> filters;
 
   const FiltersLoaded(this.filters);
+
+  @override
+  List<Object?> get props => [filters];
 }
