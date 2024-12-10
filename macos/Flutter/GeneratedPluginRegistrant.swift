8996a3006dc62b316fa92050db4e5642ae63d209
MarekJAM <marekash@gmail.com>
2024-12-10 19:01:43 +0100

    Packages bump

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import flutter_local_notifications
 import flutter_timezone
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
